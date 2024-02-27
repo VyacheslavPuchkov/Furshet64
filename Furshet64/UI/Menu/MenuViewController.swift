bf95cd7ba6991b781a9c313dbc22c9122c02cfac
VyacheslavPuchkov <Slavik-018@yandex.ru>
2024-02-27 18:53:56 +0400

    bug fixes

--- Furshet64/UI/Menu/MenuViewController.swift
+++ Furshet64/UI/Menu/MenuViewController.swift
@@ -24,7 +24,7 @@ class MenuViewController: UIViewController {
         return tableView
     }()
     
- //   var header: MenuTypeView?
+    var header: MenuTypeView?
     
     // MARK: - Init
     init() {
